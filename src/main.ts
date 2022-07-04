c41c46d8e480be846ab1fb0338dee3f5acabc69b
Michael <mradionov.oss@gmail.com>
2022-07-04 22:56:07 +1000

    Refactor to make code readable

--- src/main.ts
+++ src/main.ts
@@ -42,58 +42,60 @@ const images = {
 async function main() {
   images.road2 = await loadImage('data/graphics/road2.png');
 
-  road();
+  draw();
 }
 
-async function road() {
+async function draw() {
   if (getKeys().includes(Keycodes.Up)) {
     state.moveOffset += speed;
   } else if (getKeys().includes(Keycodes.Down)) {
     state.moveOffset -= speed;
   }
 
+  // Sky
   context.fillStyle = '#88a';
   context.fillRect(0, 0, IMAGE_WIDTH, HORIZON_Y);
 
+  // Ground
   context.fillStyle = '#aa8';
   context.fillRect(0, HORIZON_Y, IMAGE_WIDTH, IMAGE_HEIGHT - HORIZON_Y);
 
-  straight();
+  drawStraightRoad();
 
-  requestAnimationFrame(road);
+  requestAnimationFrame(draw);
 }
 
-function straight() {
-  const hors = getHors();
-  const vers = getVerts();
+function drawStraightRoad() {
+  const widthList = getWidthList();
+  const heightList = getSegmentHeightList();
 
-  const getTextureIndex = (y) => {
-    for (let i = 0; i < vers.length; i++) {
-      if (vers[i][0] <= y && vers[i][1] >= y) {
-        return vers[i][3];
+  const getTextureIndexForY = (y) => {
+    for (let i = 0; i < heightList.length; i++) {
+      const heightEntry = heightList[i];
+      if (heightEntry.y <= y && heightEntry.y2 >= y) {
+        return heightEntry.textureIndex;
       }
     }
+    throw new Error(`Could not find height entry for y=${y}`);
   };
 
-  const textures = [
-    [0, 1],
-    [33, 1],
+  const textureSources = [
+    { y: 0, height: 1 },
+    { y: 33, height: 1 },
   ];
 
-  let verIndex = 0;
-
-  for (let i = 0; i <= ROAD_HEIGHT; i++) {
+  for (let y = 0; y <= ROAD_HEIGHT; y++) {
     const sourceX = 0;
     const sourceW = ROAD_IMAGE_WIDTH;
 
-    const [destX, destW] = hors[i];
+    const { x: destX, width: destW } = widthList[y];
 
-    const ti = getTextureIndex(i);
+    const textureIndex = getTextureIndexForY(y);
 
-    const sourceY = textures[ti][0];
-    const sourceH = textures[ti][1];
+    const sourceY = textureSources[textureIndex].y;
+    const sourceH = textureSources[textureIndex].height;
 
-    const destY = IMAGE_HEIGHT - i;
+    const destY = IMAGE_HEIGHT - y;
     const destH = 1;
 
     context.drawImage(
@@ -110,68 +112,107 @@ function straight() {
   }
 }
 
-function getHors() {
+function getWidthList() {
   let dx = 0;
   const dxx = 1.6;
   const NEAR_WIDTH = 400;
 
-  const widths = [];
+  const widthList = [];
 
   for (let i = 0; i <= ROAD_HEIGHT; i++) {
     const x = IMAGE_WIDTH / 2 - NEAR_WIDTH / 2 + dx;
-    const w = NEAR_WIDTH - dx * 2;
+    const width = NEAR_WIDTH - dx * 2;
 
-    widths.push([x, w]);
+    widthList.push({
+      x,
+      width,
+    });
 
     dx += dxx;
   }
 
-  return widths;
+  return widthList;
 }
 
-function getVerts() {
-  const hs = [];
-  const vers = [];
+function getSegmentHeightList() {
+  const heightList = [];
 
-  let left = ROAD_HEIGHT;
-  let i = 1;
-  let y = 0;
+  const { moveOffset } = state;
+  const isNegativeMoveOffset = moveOffset < 0;
 
-  const ofp = Math.abs(state.moveOffset % NEAR_TEX_HEIGHT) / NEAR_TEX_HEIGHT;
-  const ofps = 1 - ofp;
+  // Will be used in a function to calculate how much the next road segment will
+  // be downscaled compared to the previous one because next segment is further
+  // into the road.
+  let downscaleIndex = 1;
 
-  const p = Math.floor(Math.abs(state.moveOffset) / NEAR_TEX_HEIGHT) % 2;
+  // Based on the nearest segment and global offset calculate how much thi
+  //  nearest segment is offset from zero position. We are going to offset
+  // all of the following segments based on the same percentages.
+  let restFillPercent =
+    Math.abs(moveOffset % NEAR_TEX_HEIGHT) / NEAR_TEX_HEIGHT;
+  let primFillPercent = 1 - restFillPercent;
 
-  let prim = !!p;
-  if (state.moveOffset < 0) {
-    prim = !prim;
+  // If we are going below zero swap the percentages because the other texture
+  // will be rendered first
+  if (isNegativeMoveOffset) {
+    primFillPercent = 1 - primFillPercent;
+    restFillPercent = 1 - restFillPercent;
   }
 
-  while (left >= 0) {
-    const m = 1.3 / i;
-    const h = Math.round(m * NEAR_TEX_HEIGHT);
-
-    const t1 = h * (state.moveOffset < 0 ? ofp : ofps);
-    const t2 = h - t1;
-
-    if (t1 !== 0) {
-      hs.push([y, y + t1, t1, prim ? 1 : 0]);
-    }
-    if (t2 !== 0) {
-      hs.push([y + t1, y + h, t2, prim ? 0 : 1]);
-    }
-
-    left -= h;
-    y += h;
-    i++;
-    prim = !prim;
-
-    if (left <= 0) {
-      break;
-    }
+  // Figure out which texture is rendered first in the current loop based on the
+  // global offset
+  let primTextureIndex =
+    Math.floor(Math.abs(state.moveOffset) / NEAR_TEX_HEIGHT) % 2;
+  // If we are going negative choose the other texture
+  if (isNegativeMoveOffset) {
+    primTextureIndex = 1 - primTextureIndex;
   }
 
-  return hs;
+  let currentY = 0;
+  let roadLeftToParse = ROAD_HEIGHT;
+
+  while (roadLeftToParse >= 0) {
+    const downscaleMultiplier = 1.3 / downscaleIndex;
+    const segmentHeight = Math.round(downscaleMultiplier * NEAR_TEX_HEIGHT);
+
+    // Segment is split into two sub-segments based on the global offset.
+    // Each segment has it's own texture.
+    const primTextureHeight = segmentHeight * primFillPercent;
+    const restTextureHeight = segmentHeight - primTextureHeight;
+
+    // Add both sub-segments as separate entries of their own height with
+    // corresponding texture indexes
+    if (primTextureHeight !== 0) {
+      heightList.push({
+        y: currentY,
+        y2: currentY + primTextureHeight,
+        height: primTextureHeight,
+        textureIndex: primTextureIndex,
+      });
+    }
+    if (restTextureHeight !== 0) {
+      heightList.push({
+        y: currentY + primTextureHeight,
+        y2: currentY + segmentHeight,
+        height: restTextureHeight,
+        textureIndex: 1 - primTextureIndex,
+      });
+    }
+
+    roadLeftToParse -= segmentHeight;
+    currentY += segmentHeight;
+    downscaleIndex++;
+    // Alernate to the other texture and make it primary
+    primTextureIndex = 1 - primTextureIndex;
+  }
+
+  return heightList;
 }
 
 main();
+
+// @ts-ignore
+window.move = (moveOffset) => {
+  state.moveOffset = moveOffset;
+  draw();
+};
