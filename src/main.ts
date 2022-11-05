7b344bf6862b82a529012d8381b94de43b955678
Michael <mradionov.oss@gmail.com>
2022-11-05 17:20:24 +1100

    Left and right turn in config

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { Keycodes, listenKeyboard } from './controls';
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const IMAGE_WIDTH = 380; // half = 190
-const IMAGE_HEIGHT = 200; // half = 100
+const IMAGE_WIDTH = 380;
+const IMAGE_HEIGHT = 200;
+const WH = IMAGE_WIDTH / 2; // half = 190
+const HH = IMAGE_HEIGHT / 2; // half = 100
 
 canvas.width = IMAGE_WIDTH;
 canvas.height = IMAGE_HEIGHT;
@@ -30,39 +32,60 @@ const straightFragment: Fragment = {
   end: 0,
 };
 
-const turnRightFragments: Fragment[] = [
-  {
-    ...straightFragment,
-    end: 100,
-  },
-  {
-    left: [180, 95, 210, 100],
-    right: [170, 95, 220, 100],
-    end: 200,
-  },
-  {
-    left: [180, 95, 305, 100],
-    right: [220, 100, 305, 100],
-    end: 300,
-  },
-  {
-    left: [180, 100, 270, 100],
-    right: [200, 105, 305, 100],
-    end: 400,
-  },
-  {
-    ...straightFragment,
-    end: 500,
-  },
-];
+function createTurn({
+  size,
+  direction,
+}: {
+  size: number;
+  direction: 'right' | 'left';
+}): Fragment[] {
+  console.assert(size >= 600, 'turn too quick: %d', size);
+  const fragments: Fragment[] = [
+    {
+      left: [180, 95, 180, 100],
+      right: [380, 150, 200, 100],
+      end: 100,
+    },
+    {
+      left: [180, 95, 210, 100],
+      right: [170, 95, 220, 100],
+      end: 200,
+    },
+    {
+      left: [180, 95, 305, 100],
+      right: [220, 100, 305, 100],
+      end: 300,
+    },
+    {
+      left: [180, 100, 270, 100],
+      right: [200, 105, 305, 100],
+      end: size - 200,
+    },
+    {
+      left: [200, 100, 250, 100],
+      right: [270, 120, 290, 100],
+      end: size - 100,
+    },
+    {
+      ...straightFragment,
+      end: size,
+    },
+  ];
+
+  if (direction === 'left') {
+    return mirrorFragments(fragments);
+  }
+
+  return fragments;
+}
 
 const state = {
-  speed: 2,
+  speed: 3,
   moveOffset: 0,
 };
 
 type Section = {
-  kind: 'straight' | 'turn-right';
+  kind: 'straight' | 'turn-right' | 'turn-left';
   start: number;
   size: number;
 };
@@ -72,12 +95,29 @@ const config: {
 } = {
   sections: [
     {
+      kind: 'turn-left',
+      start: 100,
+      size: 600,
+    },
+    {
       kind: 'turn-right',
-      start: 200,
-      size: 500,
+      start: 700,
+      size: 1000,
     },
   ],
 };
+
+function mirrorFragments(fragments: Fragment[]): Fragment[] {
+  return fragments.map((f) => {
+    const l = f.left;
+    const r = f.right;
+    return {
+      ...f,
+      left: [WH + (WH - r[0]), r[1], WH + (WH - r[2]), r[3]],
+      right: [WH + (WH - l[0]), l[1], WH + (WH - l[2]), l[3]],
+    };
+  });
+}
 
 function hasSectionEnded(section: Section) {
   return section.start + section.size < state.moveOffset;
@@ -89,8 +129,22 @@ function draw() {
   grid();
   horizon();
 
+  // drawPath({ ...straightFragment, color: 'yellow' });
+  // drawPath({ ...turn1Fragments[3], color: 'blue' });
+
+  // drawPath(turn1Fragments[4]);
+  // return;
+
+  // const f1 = turn1Fragments[3];
+  // const f2 = turn1Fragments[4];
+
+  // const path = transitionFragments(f1, f2, f1.end + state.moveOffset);
+
+  // drawPath(path);
+  // return;
+
   let activeSection: Section = config.sections.find((s) => {
-    return state.moveOffset >= s.start;
+    return state.moveOffset >= s.start && state.moveOffset <= s.start + s.size;
   });
   if (!activeSection || hasSectionEnded(activeSection)) {
     activeSection = { start: state.moveOffset, kind: 'straight', size: 0 };
@@ -103,16 +157,24 @@ function draw() {
     return;
   }
 
-  if (activeSection.kind === 'turn-right') {
+  if (
+    activeSection.kind === 'turn-right' ||
+    activeSection.kind === 'turn-left'
+  ) {
     const inSectionOffset = state.moveOffset - activeSection.start;
 
-    const activeIndex = turnRightFragments.findIndex((fragment) => {
+    const turnFragments = createTurn({
+      size: activeSection.size,
+      direction: activeSection.kind === 'turn-right' ? 'right' : 'left',
+    });
+
+    const activeIndex = turnFragments.findIndex((fragment) => {
       return inSectionOffset < fragment.end;
     });
     const prevIndex = activeIndex !== -1 ? activeIndex - 1 : -1;
 
-    const prevFragment = turnRightFragments[prevIndex] || straightFragment;
-    const activeFragment = turnRightFragments[activeIndex] || straightFragment;
+    const prevFragment = turnFragments[prevIndex] || straightFragment;
+    const activeFragment = turnFragments[activeIndex] || straightFragment;
 
     let d = 0;
 
@@ -126,6 +188,22 @@ function draw() {
     drawPath(path);
     return;
   }
+}
+
+function transitionFragments(
+  f1: Fragment,
+  f2: Fragment,
+  inSectionOffset: number,
+): PathDescriptor {
+  const size = f2.end - f1.end;
+  const internalOffset = inSectionOffset - f1.end;
+
+  let d = 0;
+  if (size !== 0) {
+    d = internalOffset / size;
+  }
+
+  return lerpPath(f1, f2, d);
 }
 
 function lerpPath(
@@ -181,8 +259,12 @@ function info({ section }: { section: string }) {
   ctx.strokeText(`section kind: ${section}`, 5, 20);
 }
 
-function drawPath({ left, right }: PathDescriptor) {
-  ctx.strokeStyle = 'red';
+function drawPath({
+  left,
+  right,
+  color = 'red',
+}: PathDescriptor & { color?: string }) {
+  ctx.strokeStyle = color;
   ctx.setLineDash([10]);
   ctx.lineDashOffset = state.moveOffset;
 
