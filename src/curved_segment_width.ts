09cb9c805d205841a47c209c14a447fcd4aa12dc
Michael <mradionov.oss@gmail.com>
2022-11-05 15:03:09 +1100

    Use lines instead

--- src/curved_segment_width.ts
+++ src/curved_segment_width.ts
@@ -1,19 +1,22 @@
 const turnSize = 500;
+
 const list = [
-  // [turnSize, 100, 0.2],
+  [turnSize, 100, 0.2],
   // [turnSize, 90, 0.2],
-  [turnSize, 80, 0.2],
+  // [turnSize, 80, 0.2],
   // [turnSize, 70, 0.2],
   // [turnSize, 60, 0.05],
-  // [turnSize, 60, 0.1],
   // [turnSize, 50, 0.05],
   // [turnSize, 40, 0.05],
   // [turnSize, 30, 0.05],
+  // [turnSize, 30, 0.05],
+  // [200, 100, 1],
+  // [100, 110, 5],
   [0, 120, 0],
 ];
 
 function lerp(entryIndex: number, offset: number) {
-  const entry = list[entryIndex];
+  const entry = list[entryIndex] || list[0];
   const nextEntry = list[entryIndex - 1] || list[0];
 
   const into = offset - entry[0];
@@ -52,11 +55,11 @@ export function getCurvedWidthList({
   let curveTopOffsetMult = 0;
 
   if (isEntering) {
-    console.log('isEntering');
     const offset = Math.max(0, moveOffset - turnStart);
     const entryIndex = list.findIndex((e) => e[0] < offset);
     const entry = list[entryIndex];
     const l = lerp(entryIndex, offset);
+    console.log('isEntering', { entryIndex });
 
     curveTopStart = l[1];
     curveTopOffsetMult = l[2];
--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,205 @@
 import { Keycodes, listenKeyboard } from './controls';
-import { getSegmentHeightList } from './segment_height';
-import { getStraightWidthList } from './straight_segment_width';
-import { getCurvedWidthList } from './curved_segment_width';
-import { installDebug } from './install_debug';
 
 const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 
-const IMAGE_WIDTH = 320;
-const IMAGE_HEIGHT = 240;
-
-const ROAD_IMAGE_WIDTH = 192;
+const IMAGE_WIDTH = 380; // half = 190
+const IMAGE_HEIGHT = 200; // half = 100
 
 canvas.width = IMAGE_WIDTH;
 canvas.height = IMAGE_HEIGHT;
 
-const { getKeys } = listenKeyboard();
-
-const state = {
-  speed: 3,
-  moveOffset: 0,
-  nextTurn: undefined,
-};
-
-const images = {
-  road2: undefined,
-};
-
-const { updateMoveOffsetLabel } = installDebug({
-  onMove: move,
-  onAddTurn: addTurn,
-  onSpeedChange: (speed: number) => {
-    state.speed = speed;
-  },
+canvas.addEventListener('click', (ev) => {
+  console.log(ev.clientX / 2, ev.clientY / 2);
 });
 
-async function loadImage(imagePath: string) {
-  return new Promise<HTMLImageElement>((resolve) => {
-    const image = new Image();
-    image.src = imagePath;
-    image.addEventListener('load', () => {
-      resolve(image);
-    });
-  });
+const { getKeys } = listenKeyboard();
+
+type LineDescriptor = [number, number, number, number];
+
+type PathDescriptor = {
+  left: LineDescriptor;
+  right: LineDescriptor;
+};
+
+type Fragment = PathDescriptor & { end: number };
+
+const straightFragment: Fragment = {
+  left: [0, 150, 180, 100],
+  right: [380, 150, 200, 100],
+  end: 0,
+};
+
+const turnRightFragments: Fragment[] = [
+  {
+    ...straightFragment,
+    end: 100,
+  },
+  {
+    left: [180, 95, 210, 100],
+    right: [170, 95, 220, 100],
+    end: 200,
+  },
+  {
+    left: [180, 95, 305, 100],
+    right: [220, 100, 305, 100],
+    end: 300,
+  },
+  {
+    left: [180, 100, 270, 100],
+    right: [200, 105, 305, 100],
+    end: 400,
+  },
+  {
+    ...straightFragment,
+    end: 500,
+  },
+];
+
+const state = {
+  speed: 2,
+  moveOffset: 0,
+};
+
+type Section = {
+  kind: 'straight' | 'turn-right';
+  start: number;
+  size: number;
+};
+
+const config: {
+  sections: Section[];
+} = {
+  sections: [
+    {
+      kind: 'turn-right',
+      start: 200,
+      size: 500,
+    },
+  ],
+};
+
+function hasSectionEnded(section: Section) {
+  return section.start + section.size < state.moveOffset;
 }
 
-async function main() {
-  images.road2 = await loadImage('data/graphics/road2.png');
+function draw() {
+  ctx.clearRect(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
 
+  grid();
+  horizon();
+
+  let activeSection: Section = config.sections.find((s) => {
+    return state.moveOffset >= s.start;
+  });
+  if (!activeSection || hasSectionEnded(activeSection)) {
+    activeSection = { start: state.moveOffset, kind: 'straight', size: 0 };
+  }
+
+  info({ section: activeSection.kind });
+
+  if (activeSection.kind === 'straight') {
+    drawPath(straightFragment);
+    return;
+  }
+
+  if (activeSection.kind === 'turn-right') {
+    const inSectionOffset = state.moveOffset - activeSection.start;
+
+    const activeIndex = turnRightFragments.findIndex((fragment) => {
+      return inSectionOffset < fragment.end;
+    });
+    const prevIndex = activeIndex !== -1 ? activeIndex - 1 : -1;
+
+    const prevFragment = turnRightFragments[prevIndex] || straightFragment;
+    const activeFragment = turnRightFragments[activeIndex] || straightFragment;
+
+    let d = 0;
+
+    const fragmentSize = activeFragment.end - prevFragment.end;
+    const inFragmentOffset = inSectionOffset - prevFragment.end;
+    if (fragmentSize !== 0) {
+      d = inFragmentOffset / fragmentSize;
+    }
+
+    const path = lerpPath(prevFragment, activeFragment, d);
+    drawPath(path);
+    return;
+  }
+}
+
+function lerpPath(
+  p1: PathDescriptor,
+  p2: PathDescriptor,
+  d: number,
+): PathDescriptor {
+  return {
+    left: lerpLine(p1.left, p2.left, d),
+    right: lerpLine(p1.right, p2.right, d),
+  };
+}
+
+function lerpLine(
+  l1: LineDescriptor,
+  l2: LineDescriptor,
+  d: number,
+): LineDescriptor {
+  console.assert(d >= 0 && d <= 1, 'd must be normalized: %d', d);
+
+  const cpx = l1[0] + (l2[0] - l1[0]) * d;
+  const cpy = l1[1] + (l2[1] - l1[1]) * d;
+  const x = l1[2] + (l2[2] - l1[2]) * d;
+  const y = l1[3] + (l2[3] - l1[3]) * d;
+
+  return [cpx, cpy, x, y];
+}
+
+function horizon() {
+  ctx.strokeStyle = 'green';
+  ctx.setLineDash([]);
+
+  ctx.beginPath();
+  ctx.moveTo(0, IMAGE_HEIGHT / 2);
+  ctx.lineTo(IMAGE_WIDTH, IMAGE_HEIGHT / 2);
+  ctx.stroke();
+}
+
+function grid() {
+  ctx.setLineDash([]);
+  ctx.strokeStyle = '#cccccc77';
+
+  ctx.moveTo(IMAGE_WIDTH / 2, 0);
+  ctx.lineTo(IMAGE_WIDTH / 2, IMAGE_HEIGHT);
+  ctx.stroke();
+}
+
+function info({ section }: { section: string }) {
+  ctx.strokeStyle = '#000';
+  ctx.font = '8px serif';
+
+  ctx.strokeText(`move offset: ${state.moveOffset}`, 5, 10);
+  ctx.strokeText(`section kind: ${section}`, 5, 20);
+}
+
+function drawPath({ left, right }: PathDescriptor) {
+  ctx.strokeStyle = 'red';
+  ctx.setLineDash([10]);
+  ctx.lineDashOffset = state.moveOffset;
+
+  ctx.beginPath();
+  ctx.moveTo(0, 150);
+  ctx.quadraticCurveTo(...left);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(380, 150);
+  ctx.quadraticCurveTo(...right);
+  ctx.stroke();
+}
+
+function main() {
   loop();
 }
-
-const turnStart = 100;
-const turnEnd = 1100;
 
 function loop() {
   if (getKeys().includes(Keycodes.Up)) {
@@ -61,109 +208,9 @@ function loop() {
     state.moveOffset -= state.speed;
   }
 
-  updateMoveOffsetLabel(state.moveOffset.toString());
-
   draw();
 
   requestAnimationFrame(loop);
 }
 
-function draw() {
-  const horizonY = IMAGE_HEIGHT / 2;
-
-  // Sky
-  context.fillStyle = '#88a';
-  context.fillRect(0, 0, IMAGE_WIDTH, horizonY);
-
-  // Ground
-  context.fillStyle = '#aa8';
-  context.fillRect(0, horizonY, IMAGE_WIDTH, IMAGE_HEIGHT - horizonY);
-
-  drawRoad();
-}
-
-function drawRoad() {
-  const horizonY = IMAGE_HEIGHT / 2;
-  const roadHeight = IMAGE_HEIGHT - horizonY;
-
-  const { moveOffset, nextTurn } = state;
-
-  const widthList = getCurvedWidthList({
-    roadHeight,
-    imageWidth: IMAGE_WIDTH,
-    moveOffset,
-    turnStart: nextTurn?.start,
-    turnEnd: nextTurn?.end,
-  });
-
-  const heightList = getSegmentHeightList({
-    roadHeight,
-    moveOffset,
-  });
-
-  const getTextureIndexForY = (y) => {
-    for (let i = 0; i < heightList.length; i++) {
-      const heightEntry = heightList[i];
-      if (heightEntry.y <= y && heightEntry.y2 >= y) {
-        return heightEntry.textureIndex;
-      }
-    }
-    throw new Error(`Could not find height entry for y=${y}`);
-  };
-
-  const groundColors = ['#aa8', '#aa8'];
-
-  const textureSources = [
-    { y: 0, height: 1 },
-    { y: 33, height: 1 },
-  ];
-
-  for (let y = 0; y <= roadHeight; y++) {
-    const sourceX = 0;
-    const sourceW = ROAD_IMAGE_WIDTH;
-
-    const { x: destX, width: destW } = widthList[y];
-
-    const textureIndex = getTextureIndexForY(y);
-
-    const sourceY = textureSources[textureIndex].y;
-    const sourceH = textureSources[textureIndex].height;
-
-    const destY = IMAGE_HEIGHT - y;
-    const destH = 1;
-
-    context.fillStyle = groundColors[textureIndex];
-    context.fillRect(0, IMAGE_HEIGHT - y, IMAGE_WIDTH, 1);
-
-    context.drawImage(
-      images.road2,
-      sourceX,
-      sourceY,
-      sourceW,
-      sourceH,
-      destX,
-      destY,
-      destW,
-      destH,
-    );
-  }
-}
-
-function move(moveOffset) {
-  state.moveOffset += moveOffset;
-  draw();
-}
-
-function addTurn({ offset, size }: { offset: number; size: number }) {
-  state.nextTurn = {
-    start: state.moveOffset + offset,
-    end: state.moveOffset + offset + size,
-  };
-}
-
 main();
-
-// @ts-ignore
-window.move = (moveOffset) => {
-  move(moveOffset);
-};
